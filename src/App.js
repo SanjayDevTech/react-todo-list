0265fd37b463a5b9be9beafed7ab79b6d88eed17
SanjayDeveloper <sureshsanjay805@gmail.com>
2021-01-09 13:51:53 +0530

    Todo list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [text, setText] = useState("");
+  const [count, setCount] = useState(1);
+  const [todos, setTodos] = useState([{id: 0, title: "Sanjay"}]);
+  function addTodo() {
+    const curText = text;
+    if (curText.trim() === "") {
+      // console.log("Empty field");
+      return;
+    }
+    const curTodos = [...todos];
+    // console.log(curTodos);
+    curTodos.push({id: count, title: curText});
+    setTodos(curTodos);
+    setCount(count+1);
+  }
+
+  function deleteTodo(id) {
+    const curTodos = [...todos];
+    curTodos.splice(id, 1);
+    setTodos(curTodos);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Todolist</header>
+      <main className="App-body">
+      <div>
+      <input className="App-input" placeholder="Type something..." onChange={(e) => setText(e.target.value)} value={text}/>
+      <button className="App-btn-add" onClick={addTodo}>Add #{count}</button>
+      </div>
+      <ul className="App-list-container">
+      {todos.map((todo, index) => (
+        <li key={todo.id} className="App-item">
+        <div className="App-item-content">
+        #{todo.id} {todo.title}
+        </div>
+        <button onClick={() => deleteTodo(index)} className="App-btn-delete">Delete #{todo.id}</button>
+        </li>
+      ))}
+      </ul>
+      </main>
     </div>
   );
 }
